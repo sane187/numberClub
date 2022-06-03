10ac08396397ea885f1e70d0d719b965c1c77a5d
Arpit joshi <joshiarpit75@gmail.com>
2022-06-03 13:54:51 +0530

    comm-aj

--- Js/index.js
+++ Js/index.js
@@ -492,4 +492,47 @@ Array.from(btnHeartArr).forEach(link => {
   });
 });
   
-  
+function format(input){
+  if(input.value < 0) input.value=Math.abs(input.value);
+  if(input.value.length > 2) input.value = input.value.slice(0, 2);
+  $(input).blur(function() {
+     // if(input.value.length == 1) input.value=0+input.value;
+     // if(input.value.length == 0) input.value='01';
+     //* if you want to allow insert only 2 digits *//
+  });
+}
+
+
+function getVals() {
+  // Get slider values
+  var parent = this.parentNode;
+  var slides = parent.getElementsByTagName("input");
+  var slide1 = parseFloat(slides[0].value);
+  var slide2 = parseFloat(slides[1].value);
+
+  // Neither slider will clip the other, so make sure we determine which is larger
+  if (slide1 > slide2) {
+    var tmp = slide2;
+    slide2 = slide1;
+    slide1 = tmp;
+  }
+
+  var displayElement = parent.getElementsByClassName("rangeValues")[0];
+  displayElement.innerHTML = slide1 + " - " + slide2;
+}
+
+window.onload = function () {
+  // Initialize Sliders
+  var sliderSections = document.getElementsByClassName("range-slider");
+  for (var x = 0; x < sliderSections.length; x++) {
+    var sliders = sliderSections[x].getElementsByTagName("input");
+    for (var y = 0; y < sliders.length; y++) {
+      if (sliders[y].type === "range") {
+        sliders[y].oninput = getVals;
+        // Manually trigger event first time to display values
+        sliders[y].oninput();
+      }
+    }
+  }
+};
+
