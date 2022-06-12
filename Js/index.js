97526b5cb2dd34eb6e6342995cf94c2e1b297637
Mohit <mohitramola98@gmail.com>
2022-06-12 19:34:01 +0530

    added wishlist

--- Js/index.js
+++ Js/index.js
@@ -358,6 +358,90 @@ Total-<strong class="mx-2">37</strong> | Sum- <strong class="mx-2">8</strong> | 
   });
 });
 
+let wishArr = [1, 2];
+let cardWish1 = document.querySelectorAll(".card-wish-row");
+const cardWish = [...cardWish1];
+cardWish.map((item) => {
+  wishArr.forEach((val) => {
+    let dov = document.createElement("div");
+    dov.classList.add("col-6", "col-md-6", "col-18", "pull-up");
+    dov.innerHTML = `  <div style="border-radius: 10px;background:linear-gradient( 40deg, #bf68e6 20%, #9e48cd 51%, #bf68e6 90% );border:1.5px solid #e6e6e6;box-shadow:0 0 10px grey;position:relative">
+      
+      <div class="d-flex justify-content-between align-items-center col-div my-1 px-2">
+          <i class="fa-solid fa-crown"> </i> 
+         
+          <h4 class="mb-0">₹3500/-</h4>
+        </div>
+  <div class="d-flex flex-column bg-cont">
+   
+  
+  
+  <div class="d-flex flex-column align-items-center col-div-2 mt-1">
+     
+  <h1 class="mb-0 fw-bold">82-<span style="color: rgb(21,30,82);">9095-9095</span></h1>
+  <a href="#" style="color:#000"><small>SIMILAR NUMBERS<i class="fa-solid fa-arrow-right-long ms-1"></i></small></a>
+  </div>
+  
+  <div class="d-flex justify-content-center col-div-3 py-1">
+  Total-<strong class="mx-2">37</strong> | Sum- <strong class="mx-2">8</strong> | <span class="mx-2">R2P</span>
+  </div>
+  
+  <div class="d-flex justify-content-center align-items-center card-icon-cont py-2 py-md-3">
+  
+  <button class="me-1" ><i class="fa-solid fa-heart wish-heart" ></i></button> 
+  
+  <button class="me-1"><i class="fa-solid fa-cart-shopping"></i></button> 
+ 
+  
+  </div>
+  </div>
+  </div>
+      `;
+    item.appendChild(dov);
+  });
+});
+
+let cardWish1Sm = document.querySelectorAll(".card-wish-sm-row");
+const cardWishSm = [...cardWish1Sm];
+cardWishSm.map((item) => {
+  wishArr.forEach((val) => {
+    let dov = document.createElement("div");
+    dov.classList.add("col-6", "col-md-6", "col-18", "pull-up");
+    dov.innerHTML = `  <div style="border-radius: 10px;background:linear-gradient( 40deg, #bf68e6 20%, #9e48cd 51%, #bf68e6 90% );border:1.5px solid #e6e6e6;box-shadow:0 0 10px grey;">
+      <div class="d-flex justify-content-between align-items-center col-div px-2 my-1">
+          <i class="fa-solid fa-crown"> </i> 
+          <h4 class="mb-0">₹3500/-</h4>
+        </div>
+  <div class="d-flex flex-column bg-cont">
+   
+  
+  
+  <div class="d-flex flex-column align-items-center col-div-2 mt-1">
+     
+  <h1 class="mb-0 fw-bold">82-<span style="color: rgb(21,30,82);;">9095-9095</span></h1>
+  <a href="#" style="color:#000"><small>SIMILAR NUMBERS<i class="fa-solid fa-arrow-right-long ms-1"></i></small></a>
+  </div>
+  
+  <div class="d-flex justify-content-center col-div-3 py-1">
+  Total- <strong class="mx-2">37</strong> | Sum- <strong class="mx-2">8</strong> | <span class="mx-2">R2P</span>
+  </div>
+  
+  <div class="d-flex justify-content-center align-items-center card-icon-cont py-2 py-md-3">
+  <div>
+  <button class="me-1 btn-eye"><i class="fa-solid fa-eye"></i> 0</button> 
+  <button class="me-1"><i class="fa-solid fa-heart"></i> 0</button> </div>
+  <div class="ms-md-5 ms-2">
+  <button class="me-1"><i class="fa-solid fa-cart-shopping"></i></button> 
+  <button>Buy</button> </div>
+  
+  </div>
+  </div>
+  </div>
+      `;
+    item.appendChild(dov);
+  });
+});
+
 // let min=document.querySelector(".min")
 // let max=document.querySelector(".max")
 // let range_min=document.querySelector(".range_min")
