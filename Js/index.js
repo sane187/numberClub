d3c34dbc382eb2b4214feacd8838d53fb3e68e0c
Mohit <mohitramola98@gmail.com>
2022-06-15 12:49:16 +0530

    change card layout of offer page mobile

--- Js/index.js
+++ Js/index.js
@@ -245,35 +245,40 @@ card6.map((item) => {
   Arr.forEach((val) => {
     let dov = document.createElement("div");
     dov.classList.add("col-6", "col-md-6", "col-18", "pull-up");
-    dov.innerHTML = `  <div style="border-radius: 10px;background:linear-gradient( 40deg, #bf68e6 20%, #9e48cd 51%, #bf68e6 90% );border:1.5px solid #e6e6e6;box-shadow:0 0 10px grey;position:relative">
-    
-    <div class="d-flex justify-content-around align-items-center col-div my-1 px-1">
-        <i class="fa-solid fa-crown"> </i> 
-        <div class="ribbon "><span>20% off</span></div>
-        <h4 class="mb-0">₹3500/-</h4>
+    dov.innerHTML = `  <div style="border-top-left-radius:5px;border-top-right-radius:5px;background:linear-gradient( 40deg, #bf68e6 20%, #9e48cd 51%, #bf68e6 90% );">
+    <div class="d-flex justify-content-between align-items-center col-div px-2 ">
+    <div class="d-inline-flex">
+    <i class="fa-solid fa-star" style="font-size: 12px;"></i>
+    <i class="fa-solid fa-star"  style="font-size: 12px;"></i>
+    <i class="fa-solid fa-star"  style="font-size: 12px;"></i>
+    <i class="fa-solid fa-star"  style="font-size: 12px;"></i>
+    <i class="fa-solid fa-star"  style="font-size: 12px;"></i>
+    </div>
+    <div class="ribbon"><span>20% off</span></div>
+    <button class="btn-card-heart"><i class="fa-solid fa-heart"></i></button>
+  </div>
       </div>
 <div class="d-flex flex-column bg-cont">
  
-
-
 <div class="d-flex flex-column align-items-center col-div-2 mt-1">
    
 <h1 class="mb-0 fw-bold">82-<span style="color:rgb(21,30,82);">9095-9095</span></h1>
-<a href="#" style="color:#000"><small>SIMILAR NUMBERS<i class="fa-solid fa-arrow-right-long ms-1"></i></small></a>
 </div>
 
 <div class="d-flex justify-content-center col-div-3 py-1">
-Total-<strong class="mx-2">37</strong> | Sum- <strong class="mx-2">8</strong> | <span class="mx-2">R2P</span>
+Total- <strong class="mx-1">37</strong> | Sum- <strong class="mx-1">8</strong> | <span class="mx-1">RTP</span> |
+<a href="#" class="ms-1"><span>SIMILAR NUMBERS</span></a>
 </div>
 
 <div class="d-flex justify-content-center align-items-center card-icon-cont py-2 py-md-3">
-<div>
-<button class="me-1 btn-eye"><i class="fa-solid fa-eye"></i> 0</button> 
-<button class="me-1"><i class="fa-solid fa-heart"></i> 0</button> </div>
-<div class="ms-md-5 ms-2">
-<button class="me-1"><i class="fa-solid fa-cart-shopping"></i></button> 
-<button>Buy</button> </div>
 
+<h6 class="mb-0 fw-bold">₹59,999</h6><div class="vl mx-1"></div><button class=""><i class="fa-solid fa-cart-shopping"></i></button>
+   
+    <div class="vl mx-1"></div>
+    <button class="py-2"><i class="fa-solid fa-clipboard-check d-block"></i></button>
+     <div class="vl mx-1"></div>
+    <button>Buy</button> 
+    
 </div>
 </div>
 </div>
