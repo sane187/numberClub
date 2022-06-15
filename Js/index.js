aa341294082bf9903ca2deaf1291b29cda79ad5a
Mohit <mohitramola98@gmail.com>
2022-06-15 14:24:22 +0530

    Chnage card layout in offer page

--- Js/index.js
+++ Js/index.js
@@ -40,7 +40,7 @@ card.map((item) => {
    <h4 class="mb-0 fw-bold">₹59,999</h4><div class="vl mx-1"></div><button class=""><i class="fa-solid fa-cart-shopping"></i></button>
    
     <div class="vl mx-1"></div>
-    <button class="py-2" onClick="Detail(this)"><i class="fa-solid fa-clipboard-check d-block"></i></button>
+    <button class="py-2" onClick="Detail(this)"><i class="fa-solid fa-circle-info d-block"></i></button>
      <div class="vl mx-1"></div>
     <button>Buy</button> 
     
@@ -91,7 +91,7 @@ cardSm.map((item) => {
 <div class="vl mx-1"></div>
 <button style="width:30px;height:30px"><i class="fa-solid fa-cart-shopping" style="font-size:.8rem"></i></button>
      <div class="vl mx-1"></div>
-    <button class="py-2" onClick="Detail(this)" style="width:30px; height:30px;"><i class="fa-solid fa-clipboard-check d-block" style="font-size:1rem"></i></button>
+    <button class="py-2" onClick="Detail(this)" style="width:30px; height:30px;"><i class="fa-solid fa-circle-info d-block" style="font-size:1rem"></i></button>
      <div class="vl mx-1"></div>
     <button style="width:60px; height:30px; font-size:.8rem">BUY</button> 
     
@@ -137,7 +137,7 @@ Total- <strong class="mx-1">37</strong> | Sum- <strong class="mx-1">8</strong> |
 <h6 class="mb-0 fw-bold">₹59,999</h6><div class="vl mx-1"></div><button class=""><i class="fa-solid fa-cart-shopping"></i></button>
    
     <div class="vl mx-1"></div>
-    <button class="py-2"><i class="fa-solid fa-clipboard-check d-block"></i></button>
+    <button class="py-2"><i class="fa-solid fa-solid fa-circle-info d-block"></i></button>
      <div class="vl mx-1"></div>
     <button>Buy</button> 
     
@@ -183,7 +183,7 @@ card2.map((item) => {
    <h4 class="mb-0 fw-bold">₹59,999</h4><div class="vl mx-1"></div><button class=""><i class="fa-solid fa-cart-shopping"></i></button>
    
     <div class="vl mx-1"></div>
-    <button class="py-2" onClick="Detail(this)"><i class="fa-solid fa-clipboard-check d-block"></i></button>
+    <button class="py-2" onClick="Detail(this)"><i class="fa-solid fa-circle-info d-block"></i></button>
      <div class="vl mx-1"></div>
     <button>Buy</button> 
     
@@ -203,34 +203,35 @@ card5.map((item) => {
   Arr.forEach((val) => {
     let dov = document.createElement("div");
     dov.classList.add("col-xl-3", "col-lg-3", "col-md-3", "col-18", "pull-up");
-    dov.innerHTML = `<div style="border-radius:10px;background:linear-gradient( 40deg, #bf68e6 20%, #9e48cd 51%, #bf68e6 90% );border:1.5px solid #e6e6e6;box-shadow:0 0 10px grey;position:relative">
-    <div class="badge-bg-cont">
-  </div>
-         <div class="d-flex justify-content-between align-items-center col-div px-2">
-             <i class="fa-solid fa-crown"> </i>
-             <h4 class="mb-0">₹3500/-</h4>
+    dov.innerHTML = `<div style="border-radius:10px;background:linear-gradient( 40deg, #bf68e6 20%, #9e48cd 51%, #bf68e6 90% );border:1.5px solid #e6e6e6;box-shadow:0 0 10px grey;">
+    <div class="badge-bg-cont"></div>
+         <div class="d-flex justify-content-between align-items-center col-div px-2 py-1 ">
+            
+             <div class="d-inline-flex"><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i></div>
+             <button class="btn-card-heart"><i class="fa-solid fa-heart"></i></button>
            </div>
      <div class="d-flex flex-column bg-cont">
-     <div class="d-flex flex-column align-items-center col-div-2 mt-2">
+     <div class="d-flex justify-content-center col-div-2 mt-2">
        
-    <h1 class="mb-0 fw-bold" style="font-size:1.8rem">82-<span style="color:rgb(21,30,82);">9095-9095</span></h1>
-    <a href="#" style="color:#000"><small>SIMILAR NUMBERS<i class="fa-solid fa-arrow-right-long ms-1"></i></small></a>
+    <h1 class="mb-0 fw-bold" style="font-size:2rem; margin-left:15px">8284-<span style="color:#9e48cd ;">095-909</span></h1>
+    
        
      </div>
     
     <div class="d-flex justify-content-center col-div-3 py-1">
-    Total -<strong class="mx-2">37</strong> | Sum - <strong class="mx-2">8</strong> | <span>R2P</span>
+    Total-<strong class="mx-2">37</strong> | Sum-<strong class="mx-2">8</strong> | <span class="mx-1"> RTP</span> | <a href="#" class="ms-1" style="font-size:10px;"><span>SIMILAR NUMBERS</span></a>
     </div>
     
     <div class="d-flex justify-content-center align-items-center card-icon-cont py-3">
-    <div>
-    <button class="me-2 btn-eye"><i class="fa-solid fa-eye"></i> 0</button> 
-    <button><i class="fa-solid fa-heart"></i> 0</button> </div>
-    <div class="ms-3">
-    <button class="me-2"><i class="fa-solid fa-cart-shopping"></i></button> 
-    <button>Buy</button> </div>
+  
+   <h4 class="mb-0 fw-bold">₹59,999</h4><div class="vl mx-1"></div><button class=""><i class="fa-solid fa-cart-shopping"></i></button>
+   
+    <div class="vl mx-1"></div>
+    <button class="py-2" onClick="Detail(this)"><i class="fa-solid fa-circle-info d-block"></i></button>
+     <div class="vl mx-1"></div>
+    <button>Buy</button> 
     
-    </div>
+    
     </div>
     </div>
     `;
@@ -275,7 +276,7 @@ Total- <strong class="mx-1">37</strong> | Sum- <strong class="mx-1">8</strong> |
 <h6 class="mb-0 fw-bold">₹59,999</h6><div class="vl mx-1"></div><button class=""><i class="fa-solid fa-cart-shopping"></i></button>
    
     <div class="vl mx-1"></div>
-    <button class="py-2"><i class="fa-solid fa-clipboard-check d-block"></i></button>
+    <button class="py-2"><i class="fa-solid fa-circle-info d-block"></i></button>
      <div class="vl mx-1"></div>
     <button>Buy</button> 
     
@@ -321,7 +322,7 @@ cardWish.map((item) => {
    <h4 class="mb-0 fw-bold">₹59,999</h4><div class="vl mx-1"></div><button class=""><i class="fa-solid fa-cart-shopping"></i></button>
    
     <div class="vl mx-1"></div>
-    <button class="py-2" onClick="Detail(this)"><i class="fa-solid fa-clipboard-check d-block"></i></button>
+    <button class="py-2" onClick="Detail(this)"><i class="fa-solid fa-circle-info d-block"></i></button>
      <div class="vl mx-1"></div>
     <button>Buy</button> 
     
@@ -369,7 +370,7 @@ Total- <strong class="mx-1">37</strong> | Sum- <strong class="mx-1">8</strong> |
 <h6 class="mb-0 fw-bold">₹59,999</h6><div class="vl mx-1"></div><button class=""><i class="fa-solid fa-cart-shopping"></i></button>
    
     <div class="vl mx-1"></div>
-    <button class="py-2"><i class="fa-solid fa-clipboard-check d-block"></i></button>
+    <button class="py-2"><i class="fa-solid fa-circle-info d-block"></i></button>
      <div class="vl mx-1"></div>
     <button>Buy</button> 
     
