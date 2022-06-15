1c3e353cdce010975fbe57ccbbc57ebfffd5153b
Mohit <mohitramola98@gmail.com>
2022-06-15 12:33:14 +0530

    Change card layout of wish page

--- Js/index.js
+++ Js/index.js
@@ -195,91 +195,7 @@ card2.map((item) => {
   });
 });
 
-//--------------------------------------------------//
-
-// CArd SOLD-------------------------//
-let cardSold = document.querySelectorAll(".card-sold-row");
-const card3 = [...cardSold];
-card3.map((item) => {
-  Arr2.forEach((val) => {
-    let dov = document.createElement("div");
-
-    dov.classList.add("col-xl-3", "col-lg-3", "col-md-3", "col-18", "pull-up");
-    dov.innerHTML = `
-<div style="border-radius:10px;background:linear-gradient( 40deg, #bf68e6 20%, #9e48cd 51%, #bf68e6 90% );border:1px solid #e6e6e6;box-shadow:0 0 10px grey;position: relative;">
-<figure>
-  <img src="./Assets/soldout.png" />
-</figure>
-<div class="d-flex justify-content-between align-items-center col-div px-2">
-    <i class="fa-solid fa-crown"> </i>
-    <h4 class="mb-0">₹3500/-</h4>
-  </div>
-<div class="d-flex flex-column bg-cont" style="background:#fff">
-
-
-
-<div class="d-flex flex-column align-items-center col-div-2 mt-2">
-
-<h1 class="mb-0 fw-bold">82-<span style="color: rgb(21,30,82);">9095-9095</span></h1>
-<a href="#"><small>SIMILAR NUMBERS<i class="fa-solid fa-arrow-right-long ms-1"></i></small></a>
-
-</div>
-
-<div class="d-flex justify-content-center col-div-3 py-1 mb-2" style="border-bottom-left-radius:10px ;border-bottom-right-radius:10px ;">
-Total -<strong class="mx-2">37</strong> | Sum - <strong class="mx-2">8</strong> | <span>R2P</span>
-</div>
-
-
-</div>
-</div>
-`;
-    item.appendChild(dov);
-  });
-});
-
-let cardSmSold = document.querySelectorAll(".card-sold-sm-row");
-const card4 = [...cardSmSold];
-
-card4.map((item) => {
-  Arr2.forEach((val) => {
-    let dov = document.createElement("div");
-
-    dov.classList.add("col-6", "col-md-6", "col-18", "pull-up");
-    dov.innerHTML = `
- <div style="border-radius:10px;background:linear-gradient( 40deg, #bf68e6 20%, #9e48cd 51%, #bf68e6 90% );border:1px solid #e6e6e6;box-shadow:0 0 10px grey;position: relative;">
- <figure>
-   <img src="./Assets/soldout.png" />
- </figure>
- <div class="d-flex justify-content-between align-items-center col-div px-2">
-     <i class="fa-solid fa-crown"> </i>
-     <h4 class="mb-0">₹3500/-</h4>
-   </div>
- <div class="d-flex flex-column bg-cont" style="background:#fff">
- 
- 
- 
- <div class="d-flex flex-column align-items-center col-div-2 mt-2">
- 
- <h1 class="mb-0 fw-bold">82-<span style="color:rgb(21,30,82);">9095-9095</span></h1>
- <a href="#"><small>SIMILAR NUMBERS<i class="fa-solid fa-arrow-right-long ms-1"></i></small></a>
- 
- </div>
- 
- <div class="d-flex justify-content-center col-div-3 py-1 mb-2" style="border-bottom-left-radius:10px ;border-bottom-right-radius:10px ;">
- Total -<strong class="mx-2">37</strong> | Sum - <strong class="mx-2">8</strong> | <span>R2P</span>
- </div>
- 
- 
- </div>
- </div>
- `;
-    item.appendChild(dov);
-  });
-});
-//-----------------------------------------------//
-
-//  Offer card //
-
+// offer card Desktop
 let cardOffer = document.querySelectorAll(".card-offer-row");
 const card5 = [...cardOffer];
 
@@ -322,6 +238,7 @@ card5.map((item) => {
   });
 });
 
+// offer card Mobile
 let cardSmOffer = document.querySelectorAll(".card-offer-sm-row");
 const card6 = [...cardSmOffer];
 card6.map((item) => {
@@ -365,6 +282,7 @@ Total-<strong class="mx-2">37</strong> | Sum- <strong class="mx-2">8</strong> | 
   });
 });
 
+// Wish list card Desktop
 let wishArr = [1, 2];
 let cardWish1 = document.querySelectorAll(".card-wish-row");
 const cardWish = [...cardWish1];
@@ -372,83 +290,93 @@ cardWish.map((item) => {
   wishArr.forEach((val) => {
     let dov = document.createElement("div");
     dov.classList.add("col-6", "col-md-6", "col-18", "pull-up");
-    dov.innerHTML = `  <div style="border-radius: 10px;background:linear-gradient( 40deg, #bf68e6 20%, #9e48cd 51%, #bf68e6 90% );border:1.5px solid #e6e6e6;box-shadow:0 0 10px grey;position:relative">
+    dov.innerHTML = `<div style="border-radius:10px;background:linear-gradient( 40deg, #bf68e6 20%, #9e48cd 51%, #bf68e6 90% );border:1.5px solid #e6e6e6;box-shadow:0 0 10px grey;">
+         <div class="d-flex justify-content-between align-items-center col-div px-2 py-1 ">
+            
+             <div class="d-inline-flex"><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i></div>
+             <button class="btn-card-heart"><i class="fa-solid fa-heart"></i></button>
+           </div>
+     <div class="d-flex flex-column bg-cont">
       
-      <div class="d-flex justify-content-between align-items-center col-div my-1 px-2">
-          <i class="fa-solid fa-crown"> </i> 
-         
-          <h4 class="mb-0">₹3500/-</h4>
-        </div>
-  <div class="d-flex flex-column bg-cont">
-   
-  
-  
-  <div class="d-flex flex-column align-items-center col-div-2 mt-1">
      
-  <h1 class="mb-0 fw-bold">82-<span style="color: rgb(21,30,82);">9095-9095</span></h1>
-  <a href="#" style="color:#000"><small>SIMILAR NUMBERS<i class="fa-solid fa-arrow-right-long ms-1"></i></small></a>
-  </div>
+    
+     <div class="d-flex justify-content-center col-div-2 mt-2">
+       
+    <h1 class="mb-0 fw-bold" style="font-size:2rem">8284-<span style="color:#9e48cd ;">095-909</span></h1>
+    
+       
+     </div>
+    
+    <div class="d-flex justify-content-center col-div-3 py-1">
+    Total-<strong class="mx-2">37</strong> | Sum-<strong class="mx-2">8</strong> | <span class="mx-1"> RTP</span> | <a href="#" class="ms-1" style="font-size:12px;"><span>SIMILAR NUMBERS</span></a>
+    </div>
+    
+    <div class="d-flex justify-content-center align-items-center card-icon-cont py-3">
   
-  <div class="d-flex justify-content-center col-div-3 py-1">
-  Total-<strong class="mx-2">37</strong> | Sum- <strong class="mx-2">8</strong> | <span class="mx-2">R2P</span>
-  </div>
-  
-  <div class="d-flex justify-content-center align-items-center card-icon-cont py-2 py-md-3">
-  
-  <button class="me-1" ><i class="fa-solid fa-heart wish-heart" ></i></button> 
-  
-  <button class="me-1"><i class="fa-solid fa-cart-shopping"></i></button> 
- 
-  
-  </div>
-  </div>
-  </div>
-      `;
+   <h4 class="mb-0 fw-bold">₹59,999</h4><div class="vl mx-1"></div><button class=""><i class="fa-solid fa-cart-shopping"></i></button>
+   
+    <div class="vl mx-1"></div>
+    <button class="py-2" onClick="Detail(this)"><i class="fa-solid fa-clipboard-check d-block"></i></button>
+     <div class="vl mx-1"></div>
+    <button>Buy</button> 
+    
+    
+    </div>
+    </div>
+    `;
     item.appendChild(dov);
   });
 });
 
+// Wish list card Mobile
 let cardWish1Sm = document.querySelectorAll(".card-wish-sm-row");
 const cardWishSm = [...cardWish1Sm];
 cardWishSm.map((item) => {
   wishArr.forEach((val) => {
     let dov = document.createElement("div");
     dov.classList.add("col-6", "col-md-6", "col-18", "pull-up");
-    dov.innerHTML = `  <div style="border-radius: 10px;background:linear-gradient( 40deg, #bf68e6 20%, #9e48cd 51%, #bf68e6 90% );border:1.5px solid #e6e6e6;box-shadow:0 0 10px grey;">
-      <div class="d-flex justify-content-between align-items-center col-div px-2 my-1">
-          <i class="fa-solid fa-crown"> </i> 
-          <h4 class="mb-0">₹3500/-</h4>
-        </div>
-  <div class="d-flex flex-column bg-cont">
+    dov.innerHTML = `  <div style="border-top-left-radius:5px;border-top-right-radius:5px;background:linear-gradient( 40deg, #bf68e6 20%, #9e48cd 51%, #bf68e6 90% );">
+    <div class="d-flex justify-content-between align-items-center col-div px-2 ">
+    <div class="d-inline-flex">
+    <i class="fa-solid fa-star" style="font-size: 12px;"></i>
+    <i class="fa-solid fa-star"  style="font-size: 12px;"></i>
+    <i class="fa-solid fa-star"  style="font-size: 12px;"></i>
+    <i class="fa-solid fa-star"  style="font-size: 12px;"></i>
+    <i class="fa-solid fa-star"  style="font-size: 12px;"></i>
+    </div>
+    <button class="btn-card-heart"><i class="fa-solid fa-heart"></i></button>
+  </div>
+      </div>
+<div class="d-flex flex-column bg-cont">
+ 
+<div class="d-flex flex-column align-items-center col-div-2 mt-1">
    
-  
-  
-  <div class="d-flex flex-column align-items-center col-div-2 mt-1">
-     
-  <h1 class="mb-0 fw-bold">82-<span style="color: rgb(21,30,82);;">9095-9095</span></h1>
-  <a href="#" style="color:#000"><small>SIMILAR NUMBERS<i class="fa-solid fa-arrow-right-long ms-1"></i></small></a>
-  </div>
-  
-  <div class="d-flex justify-content-center col-div-3 py-1">
-  Total- <strong class="mx-2">37</strong> | Sum- <strong class="mx-2">8</strong> | <span class="mx-2">R2P</span>
-  </div>
-  
-  <div class="d-flex justify-content-center align-items-center card-icon-cont py-2 py-md-3">
-  <div>
-  <button class="me-1 btn-eye"><i class="fa-solid fa-eye"></i> 0</button> 
-  <button class="me-1"><i class="fa-solid fa-heart"></i> 0</button> </div>
-  <div class="ms-md-5 ms-2">
-  <button class="me-1"><i class="fa-solid fa-cart-shopping"></i></button> 
-  <button>Buy</button> </div>
-  
-  </div>
-  </div>
-  </div>
-      `;
+<h1 class="mb-0 fw-bold">82-<span style="color:rgb(21,30,82);">9095-9095</span></h1>
+</div>
+
+<div class="d-flex justify-content-center col-div-3 py-1">
+Total- <strong class="mx-1">37</strong> | Sum- <strong class="mx-1">8</strong> | <span class="mx-1">RTP</span> |
+<a href="#" class="ms-1"><span>SIMILAR NUMBERS</span></a>
+</div>
+
+<div class="d-flex justify-content-center align-items-center card-icon-cont py-2 py-md-3">
+
+<h6 class="mb-0 fw-bold">₹59,999</h6><div class="vl mx-1"></div><button class=""><i class="fa-solid fa-cart-shopping"></i></button>
+   
+    <div class="vl mx-1"></div>
+    <button class="py-2"><i class="fa-solid fa-clipboard-check d-block"></i></button>
+     <div class="vl mx-1"></div>
+    <button>Buy</button> 
+    
+</div>
+</div>
+</div>
+    `;
     item.appendChild(dov);
   });
 });
 
+// function to add card to wish list
 let btnHeartArr = document.querySelectorAll(".btn-card-heart");
 let bool1 = false;
 
