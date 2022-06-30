348910ba9f33d2ab1d010bf62c6bd99cdcab9e24
unknown <mohitramola98@gmail.com>
2022-06-30 23:27:59 +0530

    started api integration in filter section

--- Js/store.js
+++ Js/store.js
@@ -3,6 +3,92 @@ let cardArr2 = document.querySelectorAll(".card-vip-row"); // store desktop
 const card2 = [...cardArr2]; // store desktop
 let cardVip = document.querySelectorAll(".card-vipSm-row"); // store mobile
 const VipSmCard = [...cardVip]; // store mobile
+let FilterCategories = document.querySelector(".FilterCategories");
+// fatch category data Desktop
+window.Check = async function (id) {
+  const response = await fetch(
+    config.backendEndpoint + `/fetch_single_category/1?category=${id}`
+  );
+  const data = await response.json();
+  const categoryData = data.data;
+  cardArr2[0].innerHTML = "";
+  let newData = categoryData.map((item) => {
+    const { price, sum_total, first_sum, number_status, number, product_id } =
+      item;
+    const first3 = number.slice(0, 2);
+    const reamaning = number.slice(2, 6);
+    const last = number.slice(6, 10);
+    return `<div class="col-xl-4 col-lg-4 col-md-4 col-18 pull-up">
+      <div
+        style="
+          border-radius: 10px;
+          background: linear-gradient(
+            40deg,
+            #bf68e6 20%,
+            #9e48cd 51%,
+            #bf68e6 90%
+          );
+          border: 1.5px solid #e6e6e6;
+          box-shadow: 0 0 10px grey;
+        "
+      >
+        <div
+          class="d-flex justify-content-between align-items-center col-div px-2 py-1"
+        >
+          <div class="d-inline-flex">
+            <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i
+            ><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i
+            ><i class="fa-solid fa-star"></i>
+          </div>
+          <button class="btn-card-heart">
+            <i class="fa-solid fa-heart"></i>
+          </button>
+        </div>
+        <div class="d-flex flex-column bg-cont">
+          <div class="d-flex justify-content-center col-div-2 mt-2">
+            <h1 class="mb-0 fw-bold" style="font-size: 2rem">
+              ${first3}-<span style="color: #9e48cd">${reamaning}-${last}</span>
+            </h1>
+          </div>
+
+          <div class="d-flex justify-content-center col-div-3 py-1">
+            Total-<strong class="mx-2">${sum_total}</strong> | Sum-<strong
+              class="mx-2"
+              >${first_sum}</strong
+            >
+            | <span class="mx-1">${number_status}</span> |
+            <a href="./vipNumber.html" class="ms-1" style="font-size: 10px"
+              ><span>SIMILAR NUMBERS</span></a
+            >
+          </div>
+
+          <div
+            class="d-flex justify-content-center align-items-center card-icon-cont py-3"
+          >
+            <h4 class="mb-0 fw-bold">₹${price}</h4>
+            <div class="vl mx-1"></div>
+            <button class=""><i class="fa-solid fa-cart-shopping"></i></button>
+
+            <div class="vl mx-1"></div>
+            <button
+              class="py-2"
+              onclick="window.location.href"
+              ="./Detail.html?product=${product_id}"
+            >
+              <i class="fa-solid fa-circle-info d-block"></i>
+            </button>
+            <div class="vl mx-1"></div>
+            <button>BUY</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    `;
+  });
+  newData = newData.join("");
+  cardArr2[0].innerHTML = newData;
+};
+
 // store card Desktop
 let VipCardData;
 let count = 1;
@@ -62,7 +148,91 @@ const FetchVip = async () => {
   });
 };
 FetchVip();
+// fetch category data mobile
+window.CategoryMobile = async function () {
+  let id = FilterCategories.selectedIndex + 1;
+  const response = await fetch(
+    config.backendEndpoint + `/fetch_single_category/1?category=${id}`
+  );
+  const data = await response.json();
+  const categoryData = data.data;
+  cardVip[0].innerHTML = "";
+  let newData = categoryData.map((item) => {
+    const { price, sum_total, first_sum, number_status, number, product_id } =
+      item;
+    const first3 = number.slice(0, 2);
+    const reamaning = number.slice(2, 6);
+    const last = number.slice(6, 10);
+    return `
+       <div class="col-6 col-md-6 col-18 pull-up">
+      <div
+        style="
+          border-top-left-radius: 5px;
+          border-top-right-radius: 5px;
+          background: linear-gradient(
+            40deg,
+            #bf68e6 20%,
+            #9e48cd 51%,
+            #bf68e6 90%
+          );
+        "
+      >
+        <div
+          class="d-flex justify-content-between align-items-center col-div px-2"
+        >
+          <div class="d-inline-flex">
+            <i class="fa-solid fa-star" style="font-size: 12px"></i>
+            <i class="fa-solid fa-star" style="font-size: 12px"></i>
+            <i class="fa-solid fa-star" style="font-size: 12px"></i>
+            <i class="fa-solid fa-star" style="font-size: 12px"></i>
+            <i class="fa-solid fa-star" style="font-size: 12px"></i>
+          </div>
+          <button class="btn-card-heart">
+            <i class="fa-solid fa-heart"></i>
+          </button>
+        </div>
+      </div>
+      <div class="d-flex flex-column bg-cont">
+        <div class="d-flex flex-column align-items-center col-div-2 mt-1">
+          <h1 class="mb-0 fw-bold">
+            ${first3}-<span style="color: #9e48cd">${reamaning}-${last}</span>
+          </h1>
+        </div>
 
+        <div class="d-flex justify-content-center col-div-3 py-1">
+          Total- <strong class="mx-1">${sum_total}</strong> | Sum-
+          <strong class="mx-1">${first_sum}</strong> |
+          <span class="mx-1">${number_status}</span> |
+          <a href="./vipNumber.html" class="ms-1"
+            ><span>SIMILAR NUMBERS</span></a
+          >
+        </div>
+
+        <div
+          class="d-flex justify-content-center align-items-center card-icon-cont py-2 py-md-3"
+        >
+          <h6 class="mb-0 fw-bold" style="font-size: 12px">₹${price}</h6>
+          <div class="vl mx-1"></div>
+          <button class=""><i class="fa-solid fa-cart-shopping"></i></button>
+
+          <div class="vl mx-1"></div>
+          <button
+            class="py-2"
+            onclick="window.location.href"
+            ="./Detail.html?product=${product_id}"
+          >
+            <i class="fa-solid fa-solid fa-circle-info d-block"></i>
+          </button>
+          <div class="vl mx-1"></div>
+          <button>BUY</button>
+        </div>
+      </div>
+    </div>
+      `;
+  });
+  newData = newData.join("");
+  cardVip[0].innerHTML = newData;
+};
 // Store card Mobile
 const FetchVipMobile = async () => {
   const response = await fetch(
